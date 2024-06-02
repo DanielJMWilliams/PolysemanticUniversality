d00c62a764067b0d2d6c71e5376e95022a492031
Daniel <dancart2001@gmail.com>
2024-06-02 21:16:21 +0100

    rework of graphing

---
+++ MinimalCorpus.py
@@ -0,0 +1,97 @@
+# This is a set of minimal corpuses constructed to explore intuitive relationships between words.
+class MinimalCorpus:
+    corpus_translate = [
+        ["Tak", "Yes", "Polish"],
+        ["Nie", "No", "Polish"],
+        ["Oui", "Yes", "French"],
+        ["Non", "No", "French"],
+        ["Ja", "Yes", "German"],
+        ["Nein", "No", "German"],
+    ]
+    
+    corpus_bark = [
+        ["Dog", "Bark"],
+        ["Tree", "Bark"],
+        ["Dog","Sound", "Bark"],
+        ["Tree", "Sound", "Creak"]
+    ]
+
+    corpus_food = [
+        ["Dog"],
+        ["Cat"],
+        ["Rabbit"],
+        ["Fish"],
+        ["Dog", "Fish"],
+        ["Cat", "Fish"],
+        ["Cat", "and", "Dog"],
+        ["Chips"],
+        ["Fish", "and", "Chips"],
+        ["Burger"],
+        ["Burger", "and", "Chips"],
+        ["Burger", "Dinner"],
+        ["Fish", "Dinner"],
+        ["Chips", "Dinner"],
+        ["Rabbit", "Pet"],
+        ["Fish", "Pet"],
+        ["Cat", "Pet"],
+        ["Dog", "Pet"]
+    ]
+
+    corpus_capital = [
+        ["Paris", "Capital"],
+        ["Rome", "Capital"],
+        ["Rome", "Italy"],
+        ["Paris", "France"],
+        ["Italy", "Country"],
+        ["France", "Country"],
+        ["England", "Country"],
+        ["London", "England"],
+        ["London", "Capital"],
+        ["Cardiff", "Wales"],
+        ["Wales", "Country"],
+        ["Cardiff", "Capital"],
+        ["Madrid", "Spain"],
+        ["Madrid", "Capital"],
+        ["Spain", "Country"],
+        ["Edinburgh", "Scotland"],
+        ["Scotland", "Country"],
+        ["Edinburgh", "Capital"],
+        ["Greece", "Athens"],
+        ["Athens", "Capital"],
+        ["Greece", "Country"]
+    ]
+
+    corpus_capital_small = [
+        ["Paris", "Capital"],
+        ["Rome", "Capital"],
+        ["Rome", "Italy"],
+        ["Paris", "France"],
+        ["Italy", "Country"],
+        ["France", "Country"]
+    ]
+
+    corpus_2 = [
+        ["One"],
+        ["Two"]
+    ]
+
+    corpus_3 = [
+        ["One"],
+        ["Two"],
+        ["Three"]
+    ]
+
+    corpus_4 = [
+        ["One"],
+        ["Two"],
+        ["Three"],
+        ["Four"]
+    ]
+
+    corpus_5 = [
+        ["One"],
+        ["Two"],
+        ["Three"],
+        ["Four"],
+        ["Five"]
+    ]
---
+++ Utils.py
@@ -0,0 +1,80 @@
+import numpy as np
+from gensim.models import Word2Vec
+import matplotlib.pyplot as plt
+
+class VectorUtils:
+    def angle_between_vectors(v1, v2):
+        dot_product = np.dot(v1, v2)
+        norm_v1 = np.linalg.norm(v1)
+        norm_v2 = np.linalg.norm(v2)
+        cos_angle = dot_product / (norm_v1 * norm_v2)
+        angle = np.arccos(cos_angle)
+        angle_degrees = np.degrees(angle)    
+        return angle_degrees
+    
+class ModelUtils:
+    def average_angle(model, corpus):
+        # calculate average angle between every combination of words in the corpus
+        angles = []
+        for i in corpus:
+            wordi = i[0]
+            for j in corpus:
+                wordj=j[0]
+                if wordi!=wordj:
+                    angles.append(VectorUtils.angle_between_vectors(model.wv[wordi], model.wv[wordj]))
+        average_angle = sum(angles)/len(angles)
+        print("Average angle: ", average_angle)
+        return average_angle
+    
+    def train_model(corpus, dimensions):
+        # Train Word2Vec model
+        model = Word2Vec(corpus, vector_size=dimensions, window=5, min_count=1, sg=1)
+        return model
+    
+    # get distinct words in a corpus
+    def corpus_vocab(corpus):
+        return np.unique([word for sublist in corpus for word in sublist])
+    
+    def prepare_word_plots(vocab, model, dimensions_to_plot=[0,1]):
+        xs = []
+        ys = []
+        annotations = []
+        for i, word in enumerate(vocab):
+            v = model.wv[word]
+            x = v[dimensions_to_plot[0]]
+            y = v[dimensions_to_plot[1]]
+            xs.append(x)
+            ys.append(y)
+            annotations.append(word)
+
+        data = {
+            "xs": xs,
+            "ys": ys,
+            "annotations": annotations             
+        }
+        return data
+    
+class GraphUtils:
+    
+    def render_multiple_graphs(data_list, columns=3):
+       # Determine the layout based on the number of graphs
+        n_graphs = len(data_list)
+        rows = (n_graphs + columns - 1) // columns  # Calculate number of rows needed
+        
+        # Create a figure with subplots
+        fig, axs = plt.subplots(rows, columns, figsize=(5 * columns, 4 * rows))
+        for i, data in enumerate(data_list):
+            ax = fig.add_subplot(rows,columns,i+1)                            
+            if "title" in data:
+                ax.title.set_text(data["title"])
+            ax.scatter(data["xs"], data["ys"])
+            for i, word in enumerate(data["annotations"]):
+                ax.text(data["xs"][i]+0.01, data["ys"][i]+0.01, word, fontsize=12, ha='center')
+            
+            #add lines through origin
+            ax.axhline(y=0, color='r', linestyle='-')
+            ax.axvline(x=0, color='r', linestyle='-')
+
+        # Adjust layout
+        plt.tight_layout()
+        plt.show()     
